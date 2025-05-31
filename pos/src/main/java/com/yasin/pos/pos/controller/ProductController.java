12c8040fb18f0acd7800e4fd3b80f75eae0ab383
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-05-31 22:28:14 +0200

    add new product

--- pos/src/main/java/com/yasin/pos/pos/controller/ProductController.java
+++ pos/src/main/java/com/yasin/pos/pos/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.yasin.pos.pos.controller;
 
 import com.yasin.pos.pos.dao.ProductDao;
+import com.yasin.pos.pos.entity.ProductEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     @Autowired
     private ProductDao productDao;
+
+
+    //add new product
+    @PostMapping(path = "/add-product")
+    public ProductEntity addProduct(@RequestBody ProductEntity product) {
+        return this.productDao.addProduct(product);
+    }
 }
--- pos/src/main/java/com/yasin/pos/pos/dao/ProductDao.java
+++ pos/src/main/java/com/yasin/pos/pos/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.yasin.pos.pos.dao;
 
+import com.yasin.pos.pos.entity.ProductEntity;
 import com.yasin.pos.pos.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class ProductDao {
 
     @Autowired
     private ProductRepo productRepo;
+
+    public ProductEntity addProduct(ProductEntity product) {
+        return this.productRepo.save(product);
+    }
 }
