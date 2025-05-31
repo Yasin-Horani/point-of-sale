69f027d09d867950f9a79410c3bb7e82127192b7
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-05-31 13:49:20 +0200

    init SaleRepo

---
+++ pos/src/main/java/com/yasin/pos/pos/repository/SaleRepo.java
@@ -0,0 +1,8 @@
+package com.yasin.pos.pos.repository;
+
+import com.yasin.pos.pos.entity.SaleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepo extends JpaRepository<SaleEntity, Integer> {
+
+}
