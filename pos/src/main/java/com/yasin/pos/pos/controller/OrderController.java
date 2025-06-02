73cd933bc4a99ae882481c3d6bc0aec814b4a950
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-06-02 23:59:00 +0200

    Init save order Controller

--- pos/src/main/java/com/yasin/pos/pos/controller/OrderController.java
+++ pos/src/main/java/com/yasin/pos/pos/controller/OrderController.java
@@ -1,13 +1,30 @@
 package com.yasin.pos.pos.controller;
 
 import com.yasin.pos.pos.dao.OrderDao;
+import com.yasin.pos.pos.dao.SaleDao;
+import com.yasin.pos.pos.dto.OrderDto;
+import com.yasin.pos.pos.entity.OrderEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/orders")
 public class OrderController {
+
     @Autowired
     private OrderDao orderDao;
+
+    @Autowired
+    private SaleDao saleDao;
+
+    // save order
+    @PostMapping(path = "/save-order")
+    public OrderEntity saveOrder(@RequestBody List<OrderDto> order) {
+        return this.saleDao.savaSale(order);
+    }
 }
