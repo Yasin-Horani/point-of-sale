744645b1ac9baf907d71c85efe0a964cd53eb93a
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-06-03 00:01:01 +0200

    get total price

--- pos/src/main/java/com/yasin/pos/pos/dao/OrderDao.java
+++ pos/src/main/java/com/yasin/pos/pos/dao/OrderDao.java
@@ -1,12 +1,27 @@
 package com.yasin.pos.pos.dao;
 
+import com.yasin.pos.pos.dto.OrderDto;
+import com.yasin.pos.pos.entity.OrderEntity;
 import com.yasin.pos.pos.repository.OrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderDao {
 
     @Autowired
     private OrderRepo orderRepo;
+
+    public OrderEntity saveOrder(List<OrderDto> order) {
+        double totalPrice = 0.0;
+        for (OrderDto orderDto : order) {
+            totalPrice += orderDto.getProductPrice();
+        }
+        OrderEntity orderEntity = new OrderEntity();
+        orderEntity.setTotal(totalPrice);
+        return this.orderRepo.save(orderEntity);
+
+    }
 }
