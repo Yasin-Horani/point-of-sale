6cdff2a3751dc63f3123dbfdc50eff2c466d8942
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-06-04 22:05:28 +0200

    add CrossOrigin to use end point in browser

--- server/src/main/java/com/yasin/pos/pos/controller/SaleController.java
+++ server/src/main/java/com/yasin/pos/pos/controller/SaleController.java
@@ -3,6 +3,7 @@ package com.yasin.pos.pos.controller;
 import com.yasin.pos.pos.dao.SaleDao;
 import com.yasin.pos.pos.entity.SaleEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/sales")
 public class SaleController {
     @Autowired
