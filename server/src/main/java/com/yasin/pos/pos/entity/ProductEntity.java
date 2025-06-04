9224dfb064a313bc53b8ebe8633d1ccf4c0a1afd
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-06-04 18:00:59 +0200

    clarify code

--- server/src/main/java/com/yasin/pos/pos/entity/ProductEntity.java
+++ server/src/main/java/com/yasin/pos/pos/entity/ProductEntity.java
@@ -2,7 +2,6 @@ package com.yasin.pos.pos.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
